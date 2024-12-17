2706444cc96b7dd6c204d41c330eb9484a76cec5
Rafael da Rosa <rafaeldarosa531@gmail.com>
2024-12-17 16:43:37 -0300

    semsocket

--- app.py
+++ app.py
@@ -1,5 +1,4 @@
 from flask import Flask, render_template
-from flask_socketio import SocketIO
 import threading
 import time
 import psycopg2
@@ -7,8 +6,6 @@ from datetime import datetime
 
 
 app = Flask(__name__)
-app.config['SECRET_KEY'] = 'secret!'
-socketio = SocketIO(app)
 
 # Conexão com o banco de dados
 def get_db_connection():
@@ -20,12 +17,6 @@ def get_db_connection():
     )
     return conn
 
-# Converter armadilhas antes de emitir
-def formatar_dados(armadilhas):
-    for armadilha in armadilhas:
-        if isinstance(armadilha['updated_at'], datetime):
-            armadilha['updated_at'] = armadilha['updated_at'].isoformat()
-    return armadilhas
 
 @app.route('/')
 def index():
@@ -38,35 +29,7 @@ def index():
     conn.close()
     return render_template('ignorar.html', armadilhas=armadilhas)
 
-# Thread para monitorar o banco de dados e enviar dados atualizados
-def monitorar_banco():
-    print("Iniciando monitoramento do banco...")
-    ultima_atualizacao = None
-    while True:
-        conn = get_db_connection()
-        cur = conn.cursor()
-        cur.execute('SELECT MAX(updated_at) FROM armadilhas')
-        atualizacao = cur.fetchone()[0]
-        cur.close()
-        conn.close()
-
-        if atualizacao != ultima_atualizacao:
-            print("Alteração detectada no banco. Emitindo atualização...")
-            ultima_atualizacao = atualizacao
-            conn = get_db_connection()
-            cur = conn.cursor()
-            cur.execute('SELECT * FROM armadilhas')
-            columns = [desc[0] for desc in cur.description]
-            armadilhas = [dict(zip(columns, row)) for row in cur.fetchall()]
-            cur.close()
-            conn.close()
-            print(formatar_dados(armadilhas))
-            socketio.emit('nova_dados', {'data': formatar_dados(armadilhas)})
-
-        time.sleep(5)  # Checa o banco a cada 5 segundos
-
 if __name__ == '__main__':
-    thread = threading.Thread(target=monitorar_banco)
-    thread.daemon = True
-    thread.start()
-    socketio.run(app, debug=True)
+    app.run(debug=True)
+
+
